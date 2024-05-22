40dd86db95fced44fb1da8137a263456809b9f6e
harriszintambila <https://github.com/harriszintambila/INF221.git>
2024-05-22 13:08:30 +0200

    edited room style color: grey

--- src/Components/rooms/meals.js
+++ src/Components/rooms/meals.js
@@ -1,24 +1,46 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const MealsGrid = () => {
   return (
     <div className="flex flex-col items-center p-4">
       <h1 className="text-xl font-bold mb-4">MEALS</h1>
       <div className="flex justify-center mb-6">
-        <div className="border-2 border-blue-500 p-2">
-          <img src="src/Components/images/baked-chicken-wings-asian-style-tomatoes-sauce-plate.jpg" alt="Main" className="w-64 h-auto" />
+        <div className="w-400 border-2 border-grey-500 p-2">
+          <img
+            src="/images/baked-chicken-wings-asian-style-tomatoes-sauce-plate.jpg" alt="Main" className="w-64 h-auto"
+          />
         </div>
       </div>
-      <div className="grid grid-cols-3 h-[100%] gap-4">
-        <img src="src/Components/images/chicken-wings-barbecue-sweetly-sour-sauce-picnic-summer-menu-tasty-food-top-view-flat-lay.jpg" alt="Thumbnail 1" className="border-2 border-blue-500 p-2" />
-        <img src="src/Components/images/mexican-tacos-with-beef-tomato-sauce-salsa.jpg" alt="Thumbnail 2" className="border-2 border-blue-500 p-2" />
-        <img src=".src/Components/images/pizza-with-salami-tomatoes-olives-cheese-dough-with-whole-wheat-flour-italian-food.jpg" alt="Thumbnail 3" className="border-2 border-blue-500 h-[100%] w-auto p-2" />
-        <img src="src/Components/images/pizza-with-salami-tomatoes-olives-cheese-dough-with-whole-wheat-flour-italian-food.jpg" alt="Thumbnail 4" className="border-2 border-blue-500 p-2" />
-        <img src="pexels-enginakyurt-1579253.jpg" alt="Thumbnail 5" className="border-2 border-blue-500 p-2" />
-        <img src="./rooms/pexels-enginakyurt-1579253.jpg" alt="Thumbnail 6" className="border-2 border-blue-500 p-2" />
+      <div className="border-grey-400 grid grid-cols-3 h-[100%] gap-4">
+        <img
+          src="/images/chicken-wings-barbecue-sweetly-sour-sauce-picnic-summer-menu-tasty-food-top-view-flat-lay.jpg"
+          alt="Thumbnail 1"
+          className="border-2 border-grey-00 p-2"
+        />
+        <img
+          src="/images/mexican-tacos-with-beef-tomato-sauce-salsa.jpg"
+          alt="Thumbnail 2"
+          className="border-2 border-grey-400 p-2"
+        />
+        <img
+          src="/images/pizza-with-salami-tomatoes-olives-cheese-dough-with-whole-wheat-flour-italian-food.jpg"
+          alt="Thumbnail 3"
+          className="border-2 border-grey-400 h-[100%] w-auto p-2"
+        />
+        <img
+          src="/images/pizza-with-salami-tomatoes-olives-cheese-dough-with-whole-wheat-flour-italian-food.jpg"
+          alt="Thumbnail 4"
+          className="border-2 border-grey-500 p-2"
+        />
+        <img
+          src="/images/pizza-with-salami-tomatoes-olives-cheese-dough-with-whole-wheat-flour-italian-food.jpg"
+          alt="Thumbnail 5"
+          className="border-2 border-grey-400 p-2"
+        />
+        <img
+          src="/images/pizza-with-salami-tomatoes-olives-cheese-dough-with-whole-wheat-flour-italian-food.jpg" alt="Thumbnail 6" className="border-2 border-grey-400 p-2"/>
       </div>
-      {/* <Link to="/meals">See Meals</Link> */}
     </div>
   );
 };
--- src/Components/rooms/room.js
+++ src/Components/rooms/room.js
@@ -6,17 +6,17 @@ export const ImageGrid = () => {
     <div className="flex flex-col items-center p-4">
       <h1 className="text-xl font-bold mb-4">EXPLORE MORE VISUAL DETAILS</h1>
       <div className="flex justify-center mb-6">
-        <div className="border-2 border-blue-500 p-2">
-          <img src="./src/Components/images/pillow-sofa.jpg" alt="Main" className="w-64 h-auto aspect: rounded-lg shadow-lg " />
+        <div className="w- border-2 border-grey-300 p-1">
+          <img src="/images/pillow-sofa.jpg" alt="Main" className="w-200 h- aspect: rounded-lg shadow-lg " />
         </div>
       </div>
-      <div className="grid grid-cols-3 gap-4">
-        <img src="./src/Components/images/pillow-sofa.jpg" alt="Pillow sofa" className="h-200 w-200 border-2 border-blue-500 p-2" />
-        <img src="src/Components/images/double-bed-with-pillows.jpg" alt="Double Bed" className="border-2 border-blue-500 p-2" />
-        <img src="src/Components/images/one-king-size-bed-hotel-room.jpg" alt="One King Size Bed" className="border-2 border-blue-500 p-2" />
-        <img src="src/Components/images/elegant-hotel-room-with-big-bed.jpg" alt="Elegant romm with big bed" className="border-2 border-blue-500 p-2" />
-        <img src="src/Components/images/spacious-hotel-room-with-rug.jpg" alt="Spacious room with rug 5" className="border-2 border-blue-500 p-2" />
-        <img src="src/Components/images/spacious-room-with-bed.jpg" alt="spacious room with bed" className="border-2 border-blue-500 p-2" />
+      <div className="aspeto: grid grid-cols-3 gap-3">
+        <img src="/images/pillow-sofa.jpg" alt="Pillow sofa" className="h-200 w-200 border-2 border-grey-400 p-1" />
+        <img src="/images/double-bed-with-pillows.jpg" alt="Double Bed" className="border-2 border-grey-400 p-2" />
+        <img src="/images/one-king-size-bed-hotel-room.jpg" alt="One King Size Bed" className="border-2 border-grey-400 p-1" />
+        <img src="/images/elegant-hotel-room-with-big-bed.jpg" alt="Elegant romm with big bed" className="border-2 border-grey-400 p-1" />
+        <img src="/images/spacious-hotel-room-with-rug.jpg" alt="Spacious room with rug 5" className="border-2 border-grey-400 p-1" />
+        <img src="/images/spacious-room-with-bed.jpg" alt="spacious room with bed" className="border-2 border-grey-400 p-1" />
       </div>
     </div>
   );
